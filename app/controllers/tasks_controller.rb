5dfc17368707146d9600ebdfcc71850d037fa834
Catherine Gracey <catherine.gracey@gmail.com>
2016-11-22 09:57:59 +1100

    Routes to the index page and add a new task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,20 @@
 class TasksController < ApplicationController
-  def all 
+  def all
     @tasks = Task.all
     render json: @tasks
+  end
+  def create
+    task = Task.new
+    task.title = params[:title]
+    task.desc = params[:desc]
+    task.due = params[:due]
+    task.complete = false
+    # Check this once log in has been written
+    task.user = session[:user_id]
+    if task.save
+      render json: task
+    else
+      render json: {error: "Task has failed to save."}
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get '/', to: 'pages#index'
   get '/tasks', to: 'tasks#all'
+  post '/task', to: 'tasks#create'
 end
