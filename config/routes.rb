93349d29450af95a863b265a3b7e8a3164a60288
Paul Williams <paul@pdwilliams.net>
2016-11-22 11:28:24 +1100

    resolve merge conflict

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   get '/', to: 'pages#index'
   get '/tasks', to: 'tasks#all'
   post '/task', to: 'tasks#create'
-  get 'tasks/:id', to: 'tasks#show'
+  get '/tasks/:id', to: 'tasks#show'
 end
