472cfdb9c7cebe603a51e2f511a29ea95cf570e8
mallabonikki <mallabonikki6678@gmail.com>
2016-11-22 11:34:43 +1100

    login in progress.... I wish can finish it today...

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   get '/', to: 'pages#index'
+  # get '/', to: 'sessions#new'
   get '/tasks', to: 'tasks#all'
   post '/task', to: 'tasks#create'
   get 'tasks/:id', to: 'tasks#show'
+
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+  resources :users
 end
