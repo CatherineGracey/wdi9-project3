233c21cd8761717fa62462d500a62bdefdc9bb77
Paul Williams <paul@pdwilliams.net>
2016-11-23 14:14:00 +1100

    task list style + fade-out hidden div on save

--- app/assets/javascripts/backbone/views/TaskView.js
+++ app/assets/javascripts/backbone/views/TaskView.js
@@ -35,10 +35,10 @@ var TaskView = Backbone.View.extend({
   addOne: function(model) {
     var pos = taskCollection.indexOf(model);
     var html = this.template(model.toJSON());
-    if (this.$el.find('h4').length === 0) {
+    if (this.$el.find('.task-summary').length === 0) {
       this.$el.append(html);
     } else {
-      this.$el.find('h4').eq(pos - 1).after(html);
+      this.$el.find('.task-summary').eq(pos - 1).after(html);
     }
   }
 
