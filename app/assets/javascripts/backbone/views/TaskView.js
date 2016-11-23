67b597183c37ddfa50f7c10b6ccb2f0f1b8116b8
Paul Williams <paul@pdwilliams.net>
2016-11-23 12:44:35 +1100

    render newly saved task in date order

--- app/assets/javascripts/backbone/views/TaskView.js
+++ app/assets/javascripts/backbone/views/TaskView.js
@@ -31,8 +31,13 @@ var TaskView = Backbone.View.extend({
   },
 
   addOne: function(model) {
+    var pos = taskCollection.indexOf(model);
     var html = this.template(model.toJSON());
-    this.$el.append(html);
+    if (this.$el.find('h4').length === 0) {
+      this.$el.append(html);
+    } else {
+      this.$el.find('h4').eq(pos - 1).after(html);
+    }
   }
 
 });
