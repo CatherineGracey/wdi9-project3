39d85d1624a2d07330f6f44ae5e7a7cbe496fc5a
mallabonikki <mallabonikki6678@gmail.com>
2016-11-23 12:24:43 +1100

    search task

--- app/assets/javascripts/backbone/main.js
+++ app/assets/javascripts/backbone/main.js
@@ -19,4 +19,31 @@ $().ready(function() {
   // Append rendered Task collection
   taskCollection.fetch()
   $('.task-list').append(taskView.render().el);
+
+
+  //search test
+  $('#search-task').keypress(function(event) {
+    // debugger
+      var keycode = (event.keycode ? event.keyCode : event.which);
+      if (keycode == '13') {
+        query = $(this).val();
+        console.log("query = " + query);
+        console.log("task collection = " + taskCollection);
+        $(this).val("");
+      }
+
+    // when enter is pressed, get the value of the input, and filter the taskCollection.
+
+    // render the filtered task collection
+
+  })
+
+  $('#search-task').focusout(function() {
+    $(this).val("Search Task");
+  });
+ 
+  $('#search-task').mousedown(function() {
+      $(this).val("");
+  })
+
 });
